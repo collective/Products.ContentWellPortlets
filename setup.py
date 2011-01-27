e68ca772a67008fcc30cb894cf3fe92e32d0f582
Eric Steele <ericsteele47@gmail.com>
2011-01-27 20:23:08 +0000

    Merge [14009] into trunk as that's actually more Plone 4 compatible than the plone4-compat branch.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.2'
+version = '3.0'
 
 setup(name='Products.ContentWellPortlets',
       version=version,
@@ -25,6 +25,7 @@ setup(name='Products.ContentWellPortlets',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'Plone>=4.0'
           # -*- Extra requirements: -*-
       ],
       entry_points="""
