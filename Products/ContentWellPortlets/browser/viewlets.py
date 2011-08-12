6b1b9b21927dfa0f5b1962c2f91e1559a28b028d
Eric Steele <ericsteele47@gmail.com>
2011-08-12 18:33:57 +0000

    Use the deco.gs grid to determine the width and positioning of portlets. So each portlet will fill 1/[# of portlet managers with defined portlets] of the container's width.

--- Products/ContentWellPortlets/browser/viewlets.py
+++ Products/ContentWellPortlets/browser/viewlets.py
@@ -2,47 +2,58 @@ from zope.component import getMultiAdapter
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility
+from plone.portlets.interfaces import IPortletManager
+from fractions import Fraction
 
-class PortletsAboveViewlet(ViewletBase):
-    render = ViewPageTemplateFile('templates/portletsabovecontent.pt')
-        
+class ContentWellPortletsViewlet(ViewletBase):
+
+    name = ""
+    manage_view = ""
+
     def update(self):
-        """
-        Define everything we want to call in the template
-        """
         context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
-        self.manageUrl =  '%s/@@manage-portletsabovecontent' % context_state.view_url()
-        
+        self.manageUrl =  '%s/%s' % (context_state.view_url(), self.manage_view)
+
         ## This is the way it's done in plone.app.portlets.manager, so we'll do the same
         mt = getToolByName(self.context, 'portal_membership')
         self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
 
+    def showPortlets(self):
+        return '@@manage-portlets' not in self.request.get('URL')
+        
+    def portletManagersToShow(self):
+        visibleManagers = []
+        for n in range(1,7):
+            name = 'ContentWellPortlets.%s%s' % (self.name, n)
+            mgr = getUtility(IPortletManager, name=name, context=self.context)
+            if mgr(self.context, self.request, self).visible:
+                visibleManagers.append(name)
+        
+        managers = []
+        numManagers = len(visibleManagers)
+        for counter, name in enumerate(visibleManagers):
+            pos = 'position-%s' % str(Fraction(counter, numManagers)).replace('/',':')
+            width = 'width-%s' % (str(Fraction(1, numManagers)).replace('/',':') if numManagers else 'full')
+            managers.append((name, 'cell %s %s %s' % (name.split('.')[-1], width, pos)))
+        return managers
 
-class PortletsBelowViewlet(ViewletBase):
+class PortletsAboveViewlet(ContentWellPortletsViewlet):
+    render = ViewPageTemplateFile('templates/portletsabovecontent.pt')
+
+    name = 'AbovePortletManager'
+    manage_view = '@@manage-portletsabovecontent'
+            
+
+class PortletsBelowViewlet(ContentWellPortletsViewlet):
     render = ViewPageTemplateFile('templates/portletsbelowcontent.pt')
         
-    def update(self):
-        """
-        Define everything we want to call in the template
-        """
-        context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
-        self.manageUrl =  '%s/@@manage-portletsbelowcontent' % context_state.view_url()
-        
-        ## This is the way it's done in plone.app.portlets.manager, so we'll do the same
-        mt = getToolByName(self.context, 'portal_membership')
-        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
+    name = 'BelowPortletManager'
+    manage_view = '@@manage-portletsbelowcontent'
 
 
-class FooterPortletsViewlet(ViewletBase):
+class FooterPortletsViewlet(ContentWellPortletsViewlet):
     render = ViewPageTemplateFile('templates/footerportlets.pt')
         
-    def update(self):
-        """
-        Define everything we want to call in the template
-        """
-        context_state = getMultiAdapter((self.context, self.request), name=u'plone_context_state')
-        self.manageUrl =  '%s/@@manage-footerportlets' % context_state.view_url()
-        
-        ## This is the way it's done in plone.app.portlets.manager, so we'll do the same
-        mt = getToolByName(self.context, 'portal_membership')
-        self.canManagePortlets = mt.checkPermission('Portlets: Manage portlets', self.context)
+    name = 'FooterPortletManager'
+    manage_view = '@@manage-portletsfooter'
