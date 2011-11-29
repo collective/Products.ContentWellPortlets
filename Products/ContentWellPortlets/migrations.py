b5fb1dd915eca0dd6001c0ec146224f55106abf5
Craig Haynal <cah190@psu.edu>
2011-11-29 20:29:19 +0000

    Do not explicitly place the inheaderportlets viewlet to avoid unstable ordering upon uninstall.

--- Products/ContentWellPortlets/migrations.py
+++ Products/ContentWellPortlets/migrations.py
@@ -2,9 +2,7 @@ import logging
 
 def from_2_1_to_3(context):
     context.runImportStepFromProfile('profile-Products.ContentWellPortlets:default',
-            'viewlets')
-    context.runImportStepFromProfile('profile-Products.ContentWellPortlets:default',
             'portlets')
     log = logging.getLogger("ContentWellPortlets")
-    log.info("Ran viewlets and portlets import steps")
+    log.info("Ran portlets import step")
     
