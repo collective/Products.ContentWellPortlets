56e799395802b8463bbd440d64e290351b0a565c
Craig Haynal <cah190@psu.edu>
2013-01-16 14:59:18 -0500

    Cleaner upgrade step.

--- Products/ContentWellPortlets/migrations.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def from_2_1_to_3(context):
-    context.runImportStepFromProfile('profile-Products.ContentWellPortlets:default',
-            'portlets')
-    log = logging.getLogger("ContentWellPortlets")
-    log.info("Ran portlets import step")
-    
