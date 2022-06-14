459a930670a718f1ccdf1468185eaeb3a94c77e6
Maurits van Rees <maurits@vanrees.org>
2022-06-14 16:46:57 +0200

    Updated and ran ./rebuild_i18n.sh

--- Products/ContentWellPortlets/locales/rebuild_i18n.sh
+++ Products/ContentWellPortlets/locales/rebuild_i18n.sh
@@ -5,9 +5,6 @@ rm ./rebuild_i18n.log
 i18ndude rebuild-pot --pot ./contentwellportlets.pot --create Products.ContentWellPortlets ../ ../profiles ../browser || exit 1
 i18ndude sync        --pot ./contentwellportlets.pot ./*/LC_MESSAGES/contentwellportlets.po
 
-i18ndude rebuild-pot --pot ../i18n/contentwellportlets.pot --create plone ../ ../profiles ../browser || exit 1
-i18ndude sync        --pot ../i18n/contentwellportlets.pot ../i18n/contentwellportlets-*.po
-
 WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
 ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
 FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
@@ -16,8 +13,8 @@ echo
 echo There are $ERRORS errors \(almost definitely missing i18n markup\)
 echo There are $WARNINGS warnings \(possibly missing i18n markup\)
 echo There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)
-echo For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or 
-echo Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir 
+echo For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or
+echo Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir
 
 touch ./rebuild_i18n.log
 
